881a31b5b34efa5b2cf6786b2f237bcb1e848e73
boiko_v <boiko_v@donnu.edu.ua>
2018-05-10 15:12:22 +0300

    Я родился

--- Lab_for_Bab_1/LabforBab1.cpp
+++ Lab_for_Bab_1/LabforBab1.cpp
@@ -14,7 +14,8 @@ int main()
 	//setlocale(LC_ALL, "Rus");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-
+	cout << "Бойко Владислав Леонтиевич." << endl;
+	cout << "Родился 15 сентября 1998 года в одном городе в Украине 90-тых." << endl;
 
 	system("pause");
 }
